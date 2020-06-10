0dbb25a4bcc45f299b6a38631e48703d27a33b38
shaidullin <shaidullinmansur@gmail.com>
2020-06-10 13:53:29 +0300

    Task6

--- src/Task/Task6.java
+++ src/Task/Task6.java
@@ -9,10 +9,6 @@ import java.util.Arrays;
  */
 
 public class Task6 {
-    /**
-     * Task 6 по ссылке:
-     * https://github.com/dragovx/HomeWork/blob/master/TASKS/src/Task6/Task6.java
-     */
 
     //#1
     static int bell(int n) {
